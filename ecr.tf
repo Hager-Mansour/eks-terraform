1e6c0a568ef02735634c329d946cf8e6ffb13d66
Hager-Mansour <hagermansour632003@gmail.com>
2026-02-09 14:57:01 +0200

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -12,6 +12,29 @@ resource "aws_ecr_repository" "app" {
   }
 
   tags = {
-    Name = "fullstack-app"
+    Name        = "fullstack-app-ecr"
+    Environment = "production"
+    ManagedBy   = "terraform"
   }
+}
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 20 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 20
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
