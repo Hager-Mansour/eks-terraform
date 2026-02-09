e531c179f9a4b3f32cb849f2f2329c021c6abe04
Hager-Mansour <hagermansour632003@gmail.com>
2026-02-09 12:15:01 +0200

    Create ecr.tf

---
+++ ecr.tf
@@ -0,0 +1,17 @@
+resource "aws_ecr_repository" "app" {
+  name = "fullstack-app"
+
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name = "fullstack-app"
+  }
+}
