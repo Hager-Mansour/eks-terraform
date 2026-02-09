8f2243e21410597de178d5a17b3f740ad4a29d7d
Hager-Mansour <hagermansour632003@gmail.com>
2026-02-09 15:03:37 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "eks_cluster_version" {
   description = "Kubernetes version of the EKS cluster"
   value       = module.eks.cluster_version
 }
+
+output "ecr_repository_url" {
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.app.repository_url
+}
+
+output "ecr_repository_name" {
+  description = "ECR repository name"
+  value       = aws_ecr_repository.app.name
+}
